dbd4d7c79e68cb88fbb84d8c776e91d6ffe66efe
NineStems <ninestems@icloud.com>
2025-07-07 17:46:22 +0300

    added more cases to work with ast tree for correctness logic of using interfaces

--- internal/scanner/core.go
+++ internal/scanner/core.go
@@ -73,19 +73,47 @@ func tags(in *ast.CommentGroup) []*entity.Tag {
 	return out
 }
 
+// exprToParameter converts ast expression to target string.
+func exprToParameter(in ast.Expr) string {
+	switch expr := in.(type) {
+	case *ast.Ident:
+		return expr.Name
+	case *ast.StarExpr:
+		return "*" + exprToParameter(expr.X)
+	case *ast.SelectorExpr:
+		return exprToParameter(expr.X) + "." + expr.Sel.Name
+	case *ast.ArrayType:
+		return "[]" + exprToParameter(expr.Elt)
+	case *ast.MapType:
+		return "map[" + exprToParameter(expr.Key) + "]" + exprToParameter(expr.Value)
+	case *ast.InterfaceType:
+		return "interface{}"
+	case *ast.FuncType:
+		return "func"
+	case *ast.Ellipsis:
+		return "..." + exprToParameter(expr.Elt)
+	case *ast.IndexExpr: // generic type, e.g. Foo[T]
+		return exprToParameter(expr.X) + "[" + exprToParameter(expr.Index) + "]"
+	case *ast.IndexListExpr: // generic with multiple parameters, e.g. Foo[T, U]
+		parts := make([]string, 0, len(expr.Indices))
+		for _, idx := range expr.Indices {
+			parts = append(parts, exprToParameter(idx))
+		}
+		return exprToParameter(expr.X) + "[" + strings.Join(parts, ", ") + "]"
+	default:
+		return fmt.Sprintf("%T", in) // for debugging purpose.
+	}
+}
+
 // parameter extracts parameter from input/output list of function.
 func parameter(in *ast.Field) *entity.Parameter {
 	var out entity.Parameter
-	out.SetName(in.Names[0].Name)
-	switch exp := in.Type.(type) {
-	case *ast.Ident:
-		out.SetSource(exp.Name)
-	case *ast.SelectorExpr:
-		out.SetSource(fmt.Sprintf("%s.%s", exp.X.(*ast.Ident), exp.Sel.Name))
-	case *ast.StarExpr:
-		selector := exp.X.(*ast.SelectorExpr)
-		out.SetSource(fmt.Sprintf("%s.%s", selector.X.(*ast.Ident), selector.Sel.Name))
+
+	if in.Names != nil {
+		out.SetName(in.Names[0].Name)
 	}
+
+	out.SetSource(exprToParameter(in.Type))
 
 	return &out
 }
